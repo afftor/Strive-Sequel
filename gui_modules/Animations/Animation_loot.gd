1af69a053c80cbb000819c3f95c4699c466aacbf
mikekirin <mike.kirin@hotmail.com>
2020-11-02 20:13:54 +0300

    Loot animation added

---
+++ gui_modules/Animations/Animation_loot.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+var lootdata
+
+func open(loot): #, message):
+	$AnimationPlayer.play("Loot")
+
+	lootdata = loot
+	# $Panel/RichTextLabel.bbcode_text = message
+	input_handler.ClearContainer($ScrollContainer/GridContainer)
+	var newbutton
+	yield(get_tree().create_timer(1.5), "timeout")
+	for i in loot.items:
+		newbutton = input_handler.DuplicateContainerTemplate($ScrollContainer/GridContainer)
+		i.set_icon(newbutton.get_node("TextureRect"))
+		newbutton.get_node("AnimationPlayer").play("LootButton")
+		yield(get_tree().create_timer(0.3), "timeout")
+		newbutton.get_node("Label").text = str(i.amount)
+		# if i.amount <= 1:
+		# 	newbutton.get_node("Label").hide()
+		globals.connectitemtooltip(newbutton, i)
+		globals.AddItemToInventory(i)
+	for i in loot.materials:
+		newbutton = input_handler.DuplicateContainerTemplate($ScrollContainer/GridContainer)
+		newbutton.get_node("TextureRect").texture = Items.materiallist[i].icon
+		newbutton.get_node("AnimationPlayer").play("LootButton")
+		yield(get_tree().create_timer(0.3), "timeout")
+		newbutton.get_node("Label").text = str(loot.materials[i])
+		globals.connectmaterialtooltip(newbutton, Items.materiallist[i])
+		ResourceScripts.game_res.materials[i] += loot.materials[i]
