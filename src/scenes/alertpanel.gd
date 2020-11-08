8e4c203feb7ce2a4c4236e6b17c20e3e77926782
Use After Free <use.after.free.177@gmail.com>
2020-11-08 15:41:46 +0100

    Rewrites Confirm/Cancel into a generic alert panel.
    The new alert panel takes two button labels and two functions to
    call. Confirm/Cancel is then reimplemented on top of it. This makes
    space for a later Yes/No panel.

---
+++ src/scenes/alertpanel.gd
@@ -0,0 +1,39 @@
+extends Popup
+
+var targetnode : Object
+
+# Functions to call (methods of `targetnode`); or '' (empty string)
+# for no-op.
+var left_fn  : String
+var right_fn : String
+
+func _ready() -> void:
+#warning-ignore:return_value_discarded
+	$ButtonL.connect('pressed',self, 'OnLeftButton')
+#warning-ignore:return_value_discarded
+	$ButtonR.connect('pressed', self, 'OnRightButton')
+
+func ShowConfirmCancel(TargetNode : Object, \
+                       ConfirmFunction : String, Text : String) -> void:
+        Show(TargetNode, Text, "CONFIRM", ConfirmFunction, "CANCEL", '')
+
+func Show(TargetNode : Object, Text : String, \
+          LeftLabel : String, LeftFunc : String, \
+          RightLabel : String, RightFunc : String) -> void:
+        popup()
+        $RichTextLabel.bbcode_text = Text
+        targetnode = TargetNode
+        left_fn = LeftFunc
+        right_fn = RightFunc
+        $ButtonL/Label.text = LeftLabel
+        $ButtonR/Label.text = RightLabel
+
+func OnLeftButton() -> void:
+        hide()
+        if left_fn != '':
+                targetnode.call(left_fn)
+
+func OnRightButton() -> void:
+        hide()
+        if right_fn != '':
+                targetnode.call(right_fn)
