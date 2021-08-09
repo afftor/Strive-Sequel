bfbe6a3d9408ed5af664fe917caabff7b52c8396
SphinxKingStone <Sphinx_King_Stone@mail.ru>
2021-08-10 02:45:45 +0300

    fixes for chainfo, added job tutorial

--- localization/en/main.gd
+++ localization/en/main.gd
@@ -2722,9 +2722,9 @@ var TranslationDict = {
 	CRAFTTUTORIAL7 = "If you decide to assign crafters from the Craft screen, select characters to be assigned, then click confirm.You can also select them from the occupation menu.",
 	CRAFTTUTORIAL8 = "Once you are done, at the left side of the screen you'll see new progress for the items being worked on.",
 
-	JOBTUTORIAL1 = "At your slave list you can change your character's job. Different locations have different occupation options based on the present resources and activities.",
-	JOBTUTORIAL2 = "Select an occupation from the list to see its details. Some occupations have different outputs which have to be selected in such cases. You can also adjust working conditions to modify the output.",
-	JOBTUTORIAL3 = "Production is based on the Total Productivity modifier. Different tasks have different additional modifiers, i.e. Stone or Iron Mining will also be affected by \"Collect Efficiency\". Productivity can be viewed at the character's info panel when hovering over.",
+	JOBTUTORIAL1 = "",
+	JOBTUTORIAL2 = "",
+	JOBTUTORIAL3 = "",
 
 	NOSLAVESINMARKET = "No Slaves Avaliable.",
 	RESETLOADCHARPROFESSION = "Class doesn't match current guild.\nStarting class will be reseted.",
