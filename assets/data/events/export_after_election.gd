98b74741c5cc7469cf8e7f9fb720324eaffee604
ykoc <rak3new@gmail.com>
2021-10-30 19:57:44 +0300

    quest_fix

--- assets/data/events/export_after_election.gd
+++ assets/data/events/export_after_election.gd
@@ -142,6 +142,9 @@ var data = {
 		text = [
 			{text = "", reqs = []}
 		],
+		common_effects = [
+#			{code = 'add_timed_event', value = "zephyra_artifacts_init", args = [{type = 'add_to_date', date = [1,1], hour = 8}], }, 
+			 ],
 		options = [
 			{code = "close", text = "DIALOGUECLOSE", reqs = [], dialogue_argument = 1, type = "next_dialogue", change_dialogue_type = 1}
 		]
