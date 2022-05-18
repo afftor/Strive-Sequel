30b74235e9cffda99ccfe921b4ad4697e7a22d20
ykoc <rak3new@gmail.com>
2022-05-18 03:27:47 +0300

    cross_back_fix

--- gui_modules/CharacterInfo/SlaveInfoModule.gd
+++ gui_modules/CharacterInfo/SlaveInfoModule.gd
@@ -130,18 +130,18 @@ func update_traitlist():
 				button.get_node('icon').texture = load(trdata.icon)
 			else:
 				button.get_node('icon').texture = trdata.icon
-		if trdata.tags.has('positive'):
-			button.texture_normal = load("res://assets/images/iconstraits/green.png")
-#			button.texture_normal = load("res://assets/Textures_v2/CHAR_INFO/traitpanel/button_tratis_positive.png")
-#			button.texture_hover = load("res://assets/Textures_v2/CHAR_INFO/traitpanel/button_tratis_positive_hover.png")
-		if trdata.tags.has('negative'):
-			button.texture_normal = load("res://assets/images/iconstraits/red.png")
-#			button.texture_normal = load("res://assets/Textures_v2/CHAR_INFO/traitpanel/button_tratis_negative.png")
-#			button.texture_hover = load("res://assets/Textures_v2/CHAR_INFO/traitpanel/button_tratis_negative_hover.png")
 		if trdata.has('cross') and trdata.cross:
 			button.get_node('cross').visible = true
 		else:
 			button.get_node('cross').visible = false
+			if trdata.tags.has('positive'):
+				button.texture_normal = load("res://assets/images/iconstraits/green.png")
+	#			button.texture_normal = load("res://assets/Textures_v2/CHAR_INFO/traitpanel/button_tratis_positive.png")
+	#			button.texture_hover = load("res://assets/Textures_v2/CHAR_INFO/traitpanel/button_tratis_positive_hover.png")
+			if trdata.tags.has('negative'):
+				button.texture_normal = load("res://assets/images/iconstraits/red.png")
+	#			button.texture_normal = load("res://assets/Textures_v2/CHAR_INFO/traitpanel/button_tratis_negative.png")
+	#			button.texture_hover = load("res://assets/Textures_v2/CHAR_INFO/traitpanel/button_tratis_negative_hover.png")
 		#i suggest put trait removing feature here, on button_press
 
 
