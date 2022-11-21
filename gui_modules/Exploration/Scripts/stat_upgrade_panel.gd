2f112782665cc5c716fa0982d0d90295471ce244
ykoc <rak3new@gmail.com>
2022-11-21 03:24:19 +0300

    Update stat_upgrade_panel.gd

--- gui_modules/Exploration/Scripts/stat_upgrade_panel.gd
+++ gui_modules/Exploration/Scripts/stat_upgrade_panel.gd
@@ -44,6 +44,7 @@ func show_characters_panel():
 		newbutton.get_node("icon").texture = character.get_icon_small()
 		newbutton.connect("pressed", self, "select_char", [character, 1])
 		newbutton.set_meta("slave", character)
+		globals.connectslavetooltip(newbutton, character)
 	input_handler.ClearContainer(CharList2)
 	for i in ResourceScripts.game_party.character_order:
 		var character = ResourceScripts.game_party.characters[i]
@@ -55,6 +56,7 @@ func show_characters_panel():
 		newbutton.get_node("icon").texture = character.get_icon_small()
 		newbutton.connect("pressed", self, "select_char", [character, 2])
 		newbutton.set_meta("slave", character)
+		globals.connectslavetooltip(newbutton, character)
 	$StatsPanel.hide()
 
 
