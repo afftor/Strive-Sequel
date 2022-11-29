386996652ac403996d00adad67a2d743d703064a
Nikita <ccekret@inbox.ru>
2022-11-30 01:51:56 +0300

    lang fix

--- gui_modules/Universal/Modules/ChangeLanguageMenuButtonModule.gd
+++ gui_modules/Universal/Modules/ChangeLanguageMenuButtonModule.gd
@@ -1,32 +1,16 @@
 extends MenuButton
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	text = tr("TESTNAMELOCALIZATION")
-	get_popup().add_item("EN")
-	get_popup().add_item("DE")
+	
+	for i in input_handler.TranslationData:
+		get_popup().add_item(i)
+		pass
 	
 	get_popup().connect("id_pressed",self,"_on_item_pressed")
-	
-	print(input_handler.globalsettings.ActiveLocalization)
-	pass # Replace with function body.
+	pass 
 
 func _on_item_pressed(id):
-	
-	name = tr("TESTNAMELOCALIZATION")
-	
 	var name = get_popup().get_item_text(id)
 	
 	input_handler.globalsettings.ActiveLocalization = name.to_lower()
 	input_handler.settings_save(input_handler.globalsettings)
-	print(name)
 	pass
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
