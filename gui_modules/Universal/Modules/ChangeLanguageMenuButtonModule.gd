57e47ae1636308d88699deb37c0bed8292df414c
Nikita <ccekret@inbox.ru>
2022-11-30 03:10:07 +0300

    Max Button in marketplace and SwitchLanguagePanel Fix

--- gui_modules/Universal/Modules/ChangeLanguageMenuButtonModule.gd
+++ gui_modules/Universal/Modules/ChangeLanguageMenuButtonModule.gd
@@ -1,4 +1,6 @@
 extends MenuButton
+var OkPanel
+
 func _ready():
 	
 	for i in input_handler.TranslationData:
@@ -11,6 +13,11 @@ func _ready():
 func _on_item_pressed(id):
 	var name = get_popup().get_item_text(id)
 	
+	if name == input_handler.globalsettings.ActiveLocalization:
+		return
+	
 	input_handler.globalsettings.ActiveLocalization = name.to_lower()
 	input_handler.settings_save(input_handler.globalsettings)
+	
+	OkPanel.visible = true
 	pass
