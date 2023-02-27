aaa9f01408a61bfe18e332c5d0997c81c88e5423
ykoc <rak3new@gmail.com>
2023-02-27 18:52:11 +0300

    fix_loan_planning

--- src/core/globals.gd
+++ src/core/globals.gd
@@ -1973,7 +1973,7 @@ func common_effects(effects):
 				if ResourceScripts.game_globals.difficulty != 'hard':
 					common_effects([{code = 'complete_quest', value = 'main_quest_loan'}, {code = 'remove_timed_events', value = ['loan_event1','loan_event2','loan_event3','loan_event4']}]) #stub, do not want recursion here
 			'plan_loan_event':
-				var newevent = {reqs = [], code = 'loan_event' + str(i.stage + 1)}
+				var newevent = {reqs = [], code = 'loan_event' + str(i.stage)}
 				var newreq = [{type = 'date', operant = 'eq', value = variables.base_loan_dates[i.stage]}, {type = 'hour', operant = 'eq', value = 1}]
 				newevent.reqs += newreq
 				ResourceScripts.game_progress.stored_events.timed_events.append(newevent)
