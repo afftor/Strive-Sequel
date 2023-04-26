e71c660519c51211018184fb7ef60a7874cf1bcd
SphinxKingStone <Sphinx_king_stone@mail.ru>
2023-04-26 10:56:49 +0300

    skill removal, missing textures fix

---
+++ skillbook_dead_zone.gd
@@ -0,0 +1,15 @@
+extends Node
+
+
+func can_drop_data(position, data):
+	if data != null and data is TextureButton:
+		return true
+	else:
+		return false
+
+func drop_data(position, data):
+	if data is TextureButton:
+#		data.get_node("Icon").texture = null
+		data.set_meta("Skill", null)
+		data.get_node("Icon").texture = null
+#		get_parent().update_combat_skill_panel(null)
