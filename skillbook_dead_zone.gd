74c9cbee358b825900b30a00fe2ab05f8e8abc91
SphinxKingStone <Sphinx_king_stone@mail.ru>
2023-04-26 10:59:01 +0300

    fix

--- skillbook_dead_zone.gd
+++ skillbook_dead_zone.gd
@@ -10,6 +10,6 @@ func can_drop_data(position, data):
 func drop_data(position, data):
 	if data is TextureButton:
 #		data.get_node("Icon").texture = null
-		data.set_meta("Skill", null)
+		data.set_meta("skill", null)
 		data.get_node("Icon").texture = null
 #		get_parent().update_combat_skill_panel(null)
