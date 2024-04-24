e8fd415dffe64f86bf9afdcdeea70b83854bff88
ykoc <rak3new@gmail.com>
2024-04-24 05:18:28 +0300

    generator_finished

--- src/core/dungeon_gen.gd
+++ src/core/dungeon_gen.gd
@@ -1,6 +1,6 @@
 extends Node
 
-var debug = true
+var debug = false
 
 func _ready():
 	if debug:
@@ -11,9 +11,15 @@ func _ready():
 		second_pass()
 		third_pass()
 		fourth_pass()
-		init_tuning()
 #		color_subtrees()
+		init_tuning()
 		fill_diameter()
+		if trim_diameter(10, 8):
+			print_debug_data(tuning)
+			trim_outer(8)
+		else:
+			print_debug_data(tuning)
+			print('fail')
 
 #graph
 # edges are connections, not walls
@@ -327,11 +333,122 @@ func fill_diameter():
 		print(diameter)
 
 
-func gather_amounts(limit = 3):
-	var res = {main = 0, outer = 0}
+func gather_amounts(limit = 2):
+	var res = {main = 0, outer = 0, outer_arr = [], roots_arr = []}
 	for i in range(tuning.size()):
 		if tuning[i] == 0:
 			res.main += 1
-		elif tuning[i] < limit:
+		elif tuning[i] <= limit:
 			res.outer += 1
+			if tuning[i] == limit:
+				res.outer_arr.push_back(i)
+			if tuning[i] == 1:
+				res.roots_arr.push_back(i)
+	
+	if debug:
+		print(res)
 	return res
+
+
+func trim_diameter(limit, outer_limit):
+	if diameter.size() < limit:
+		return false
+	
+	while diameter.size() > limit:
+		var rem_vertex
+		if globals.rng.randf() > 0.5:
+			rem_vertex = diameter.front()
+			diameter.pop_front()
+		else:
+			rem_vertex = diameter.back()
+			diameter.pop_back()
+		var t_arr = find_subtree(pack_vertex(rem_vertex))
+		for v in t_arr:
+			tuning[v] +=1
+	
+	var temp = gather_amounts()
+	if temp.outer >= outer_limit:
+		return true
+	else:
+		return false
+
+
+func trim_outer(outer_limits, limit = 2): #temp solution - greedy distant based triming outer vertexes
+	#more complex solution should trim full subtrees until conditions met
+	#but for current small limits there is little difference 
+	var temp = gather_amounts(1)
+	if temp.outer >= outer_limits:
+		limit = 1
+	for i in range(tuning.size()):
+		if tuning[i] > limit:
+			tuning[i] = 99
+	
+	temp = gather_amounts()
+	if temp.outer < outer_limits:
+		print("error in calling outer trimming")
+	else:
+		while temp.outer > outer_limits:
+			var t = input_handler.random_from_array(temp.outer_arr)
+			temp.outer_arr.erase(t)
+			tuning[t] = 99
+			temp.outer -= 1
+	
+	if debug:
+		print_debug_data(tuning)
+
+
+#api
+func generate(data):
+	debug = false
+	lines = 5
+	cols = 5
+	
+	if data.has('debug'):
+		debug = data.debug
+	if data.has('lines'):
+		lines = data.lines
+	if data.has('cols'):
+		cols = data.cols
+	
+	for arg in ['main_route_length', 'bonus_rooms']:
+		if data[arg] is Array:
+			data[arg] = globals.rng.randi_range(data[0], data[1])
+	
+	var main_route_length = data.main_route_length
+	var bonus_rooms = data.bonus_rooms
+	
+	var retry_attempts_global = 10
+	while retry_attempts_global > 0:
+		var retry_attempts = 4
+		var completed = false
+		prim()
+		first_pass()
+		second_pass()
+		third_pass()
+		fourth_pass()
+		
+		while retry_attempts > 0:
+			retry_attempts -= 1
+			init_tuning()
+			fill_diameter()
+			if trim_diameter(main_route_length, bonus_rooms):
+				if debug:
+					print_debug_data(tuning)
+				trim_outer(bonus_rooms)
+				completed = true
+				break
+			else:
+				if debug:
+					print_debug_data(tuning)
+					print('fail trim %d %d' % [4 - retry_attempts, 10 - retry_attempts_global])
+		if completed:
+			break
+		else:
+			print('fail seed %d' % (10 - retry_attempts_global))
+		retry_attempts_global -= 1
+	if retry_attempts_global < 0:
+		print("bad data limits")
+		print(data)
+		return false
+	else:
+		return true
