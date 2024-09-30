482e8f4b70abd9d9a8dd41be8dad7d57418a1e02
ykoc <rak3new@gmail.com>
2024-09-30 03:19:51 +0300

    nixx_champion

--- assets/data/classes.gd
+++ assets/data/classes.gd
@@ -1036,7 +1036,7 @@ var professions = {
 		showupreqs = [{code = 'disabled', check = true}],
 		reqs = [{code = 'cant_spawn_naturally'}],
 		statchanges = {mpmax = 10, hpmax = 25, resist_dark = 75, price = 500, chg_wisdom_max = 1, chg_dexterity_max = 1},
-		traits = [],
+		traits = ['nixx_champion'],
 		skills = [],
 		combatskills = [],
 		conflict_classes = [],
