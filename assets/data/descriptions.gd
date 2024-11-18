08c1857b66a5bea4af1508ac8e008b9518c05885
ykoc <rak3new@gmail.com>
2024-11-19 02:23:34 +0300

    reqs_removed

--- assets/data/descriptions.gd
+++ assets/data/descriptions.gd
@@ -726,6 +726,8 @@ func get_class_traits_no_icons(newperson, classdata):
 	var text = ''
 	for i in classdata.traits:
 		if i == "basic_spells" or i == "advanced_spells" or i == "basic_combat" or i == "advanced_combat":
+			print('error in classdata')
+			print(classdata)
 			continue
 		var trait = Traitdata.traits[i]
 		if trait.has('show_in_parent_stats') and trait.show_in_parent_stats: continue
