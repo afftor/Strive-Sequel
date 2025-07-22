c2342b8620089a75fd971754ba42577f361f47c6
Kiremon-work <elseweyr@yandex.ru>
2025-07-22 14:16:00 +0600

    manual conflict resolve for ch_leveling.gd after merge

--- src/character/ch_leveling.gd
+++ src/character/ch_leveling.gd
@@ -569,7 +569,6 @@ func select_brothel_activity():
 				parent.get_ref().take_virginity('anal', 'brothel_customer')
 			
 			work_tick_values(input_handler.random_from_array(data.workstats))
-			
 			parent.get_ref().try_rise_fame('service')
 			
 			parent.get_ref().add_stat('metrics_randompartners', globals.fastif(sex_rules.has('group'), 2, 1))
@@ -599,7 +598,6 @@ func select_brothel_activity():
 		
 		var data = tasks.gold_tasks_data[highest_value.code]
 		work_tick_values(input_handler.random_from_array(data.workstats))
-		
 		parent.get_ref().try_rise_fame('service')
 		
 		var goldearned = highest_value.value * min(4, (1 + 0.001 * parent.get_ref().calculate_price()))
