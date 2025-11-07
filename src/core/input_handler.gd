ec39d00b394c968dd83b1722df758f066cf522a7
Kiremon-work <elseweyr@yandex.ru>
2025-11-07 19:10:13 +0600

    fix for autocompleted repeatable quest's rewards

--- src/core/input_handler.gd
+++ src/core/input_handler.gd
@@ -1274,6 +1274,7 @@ func autocomplete_quest(q_id):
 	var questdata = ResourceScripts.game_world.get_quest_by_id(q_id)
 	selectedquest = questdata
 	play_animation("repeatable_quest_completed")
+	PlaySound("questcomplete")
 	globals.Reward(questdata)
 	globals.text_log_add("quest", "Quest Complete: " + questdata.name)
 	ResourceScripts.game_world.complete_quest(questdata, 'complete')
