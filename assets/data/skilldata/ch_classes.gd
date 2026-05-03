aa52da02efcd79c80384d1d74b44e731506c1e66
ykoc <rak3new@gmail.com>
2026-05-04 00:33:18 +0300

    templar_fix

--- assets/data/skilldata/ch_classes.gd
+++ assets/data/skilldata/ch_classes.gd
@@ -1448,7 +1448,7 @@ var effects = {
 	},
 	e_tr_templar_heal = {
 		type = 'trigger',
-		trigger = [variables.TR_TURN_GET],
+		trigger = [variables.TR_TURN_F],
 		req_skill = false,
 		sub_effects = [
 			{
