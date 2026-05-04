82243b3c30efe05c34f8ac03c5bced8a59d42461
ykoc <rak3new@gmail.com>
2026-05-05 00:26:51 +0300

    counterattack_upd

--- assets/data/skilldata/ch_classes.gd
+++ assets/data/skilldata/ch_classes.gd
@@ -1175,7 +1175,7 @@ var effects = {
 	},
 	e_tr_paladin_5 = {
 		type = 'trigger',
-		trigger = [variables.TR_DEF_HIT],
+		trigger = [variables.TR_POST_TARG],
 		req_skill = true,
 		conditions = [
 			{type = 'skill', value = ['tags', 'has', 'damage']},
