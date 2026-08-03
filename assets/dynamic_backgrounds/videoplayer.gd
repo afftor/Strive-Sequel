58daba7f74ec09695479f59188d8f8e3f9b2575b
Kiremon-work <elseweyr@yandex.ru>
2026-08-03 16:38:13 +0600

    fix for disabling animated backgrounds and probable fix for animated background's flicker on video restart

--- assets/dynamic_backgrounds/videoplayer.gd
+++ assets/dynamic_backgrounds/videoplayer.gd
@@ -2,26 +2,39 @@ extends VideoPlayer
 
 var current_scene
 
-var x : bool = true
+#var x : bool = true
 
-func _process(delta):
-#	print(stream_position)
-#	if input_handler.globalsettings.animatedbackground == false && self.is_playing() == false && is_visible_in_tree() == true:
-#		print('+')
-#		self.play()
-#		x = true
-	if input_handler.globalsettings.animatedbackground == true && x == true:
-		yield(get_tree().create_timer(0.7), "timeout")
-		self.stop()
-		x = false
+#func _process(delta):
+##	print(stream_position)
+##	if input_handler.globalsettings.animatedbackground == false && self.is_playing() == false && is_visible_in_tree() == true:
+##		print('+')
+##		self.play()
+##		x = true
+#	if input_handler.globalsettings.animatedbackground == true && x == true:
+#		yield(get_tree().create_timer(0.7), "timeout")
+#		self.stop()
+#		x = false
 
 func open(background):
 	if current_scene != background:
 		current_scene = background
 		stream = images.dynamic_backgrounds[background]
 		self.play()
-	show()
+		if input_handler.globalsettings.animatedbackground:
+			#video seems to need 3 frames to put some image on screen
+			#need to remake it somehow
+			yield(get_tree(), "idle_frame")
+			yield(get_tree(), "idle_frame")
+			yield(get_tree(), "idle_frame")
+			self.stop()
+#	show()
 
+func consider_stop():
+	if input_handler.globalsettings.animatedbackground:
+		self.stop()
+	else:
+		self.play()
 
 func _ready():
 	connect('finished', self, 'play')
+	input_handler.connect('animatedbackground_changed', self, 'consider_stop')
