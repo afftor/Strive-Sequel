8505acabc623e5e48f289219574e7e9a3d5ca09b
ykoc <rak3new@gmail.com>
2026-09-18 05:04:22 +0300

    monocolored_fur_fix

--- Character_generator/Doll2Spine/universal/doll_coverage.gd
+++ Character_generator/Doll2Spine/universal/doll_coverage.gd
@@ -47,8 +47,8 @@ const PATTERNS = {
 		"nipple": Color("543131"),
 		"mouth": 2,
 		"layers": [
-			{"mask": "mask_besatkin 2 ext.png", "color": Color("904b18")},
-			{"mask": "mask_besatkin 2.png", "color": Color("904b18")},
+			{"mask": null, "color": Color("904b18")},
+#			{"mask": "mask_besatkin 2.png", "color": Color("904b18")},
 		],
 	},
 	"fur_orange_white": {
@@ -67,8 +67,8 @@ const PATTERNS = {
 		"nipple": Color("6b6564"),
 		"mouth": 2,
 		"layers": [
-			{"mask": "mask_besatkin 2 ext.png", "color": Color("ababab")},
-			{"mask": "mask_besatkin 2.png", "color": Color("ababab")},
+			{"mask": null, "color": Color("ababab")},
+#			{"mask": "mask_besatkin 2.png", "color": Color("ababab")},
 		],
 	},
 	"fur_grey": {
@@ -77,8 +77,8 @@ const PATTERNS = {
 		"nipple": Color("4e4948"),
 		"mouth": 2,
 		"layers": [
-			{"mask": "mask_besatkin 2 ext.png", "color": Color("6d6d6d")},
-			{"mask": "mask_besatkin 2.png", "color": Color("6d6d6d")},
+			{"mask": null, "color": Color("6d6d6d")},
+#			{"mask": "mask_besatkin 2.png", "color": Color("6d6d6d")},
 		],
 	},
 	"fur_brown": {
@@ -87,8 +87,8 @@ const PATTERNS = {
 		"nipple": Color("493c3a"),
 		"mouth": 2,
 		"layers": [
-			{"mask": "mask_besatkin 2 ext.png", "color": Color("513b28")},
-			{"mask": "mask_besatkin 2.png", "color": Color("513b28")},
+			{"mask": null, "color": Color("513b28")},
+#			{"mask": "mask_besatkin 2.png", "color": Color("513b28")},
 		],
 	},
 	"fur_black": {
